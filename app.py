d7821b00ecf53168788e29006af71d4fb468bb52
quincy-o <quincyeodom@gmail.com>
2025-11-09 21:29:11 -0800

    Have all pages implemented in their most barebones forms. Could use a bit more work for the skeleton

--- app.py
+++ app.py
@@ -9,5 +9,26 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/profile_creation")
+def profile_creation():
+    return render_template('profile_creation.html')
+
+@app.route("/my_projects")
+def my_projects():
+    return render_template('my_projects.html')
+
+@app.route("/profile")
+def profile():
+    return render_template('profile.html')
+
+#Ideally, the functionality of this should change depening on the project itself
+@app.route("/project")
+def project():
+    return render_template('project.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
