814f7bcdf98db62716acf4a530268b92ddccb8b2
Nicholas <nick.uhlhorn@gmail.com>
2025-12-03 19:14:36 -0800

    removed project files and added blank website files

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-# Nicholas J Uhlhorn
-# November 2025
-
-# Import the model classes here
-from .user import User
-from .project import Project, ProjectMember
-from .reaction import Reaction
-from .friend import FriendRequest
-
--- services/__init__.py
+++
@@ -1,6 +0,0 @@
-# Nicholas J Uhlhorn
-# November 2025
-
-from . import user_service
-from . import project_service
-from . import reaction_service
